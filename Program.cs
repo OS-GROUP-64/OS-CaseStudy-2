d1da84d90c8e56d6cfee6e1a14fc6aa93c6c7e5c
Sirawit <popeye-zz@hotmail.com>
2021-11-04 18:02:49 +0700

    v3.0

--- Program.cs
+++ Program.cs
@@ -30,38 +30,50 @@ namespace OS_Problem_02
             return x;
         }
 
-        static void th01()
+        static void th01(object t)
         {
             int i;
             for (i = 1; i < 51; i++)
             {
                 lock (_Lock)
                 {
-                    if(Count < 10){
-                        EnQueue(i);
-                        Console.WriteLine("EnQueue i={0}, thread:{1}", i, 0);
-                        Thread.Sleep(5);
-                    }else{
-                        Console.WriteLine("EnQueue Waiting thread:{0}", 0);
+                    if (Count >= 10) // more than or equal to 10
+                    {
+                        Console.WriteLine("\n-- EnQueue Waiting thread:{0} --\n", t);
                         Monitor.Wait(_Lock);
-                        Console.WriteLine("*********EnQueue Continue*********");
                     }
-
+                    EnQueue(i);
+                    Console.WriteLine("** EnQueue i={0}, thread:{1} **", i, t);
+                    Thread.Sleep(5);
+                    if (Count >= 1){ // if has something, wake up DeQueue Thread
+                        Monitor.Pulse(_Lock);
+                    }
                 }
 
             }
         }
 
-        // static void th011()
-        // {
-        //     int i;
-
-        //     for (i = 100; i < 151; i++)
-        //     {
-        //         EnQueue(i);
-        //         Thread.Sleep(5);
-        //     }
-        // }
+        static void th011(object t)
+        {
+            int i;
+            for (i = 100; i < 151; i++)
+            {
+                lock (_Lock)
+                {
+                    if (Count >= 10) // more than or equal to 10
+                    {
+                        Console.WriteLine("\n-- EnQueue Waiting thread:{0} --\n", t);
+                        Monitor.Wait(_Lock);
+                    }
+                    EnQueue(i);
+                    Console.WriteLine("** EnQueue i={0}, thread:{1} **", i, t);
+                    Thread.Sleep(5);
+                    if (Count >= 1){ // if has something, wake up DeQueue Thread
+                        Monitor.Pulse(_Lock);
+                    }
+                }
+            }
+        }
 
 
         static void th02(object t)
@@ -73,32 +85,38 @@ namespace OS_Problem_02
             {
                 lock (_Lock)
                 {
-                    if(Count > 0){
-                        j = DeQueue();
-                        Console.WriteLine("DeQueue j={0}, thread:{1}", j, t);
-                        Thread.Sleep(100);
-                    }else{
-                        Console.WriteLine("Nothing to DeQueue thread:{0}", t);
+                    if (Count <= 0) // equal or less than 0 
+                    {
+                        Console.WriteLine("\n-- Nothing to DeQueue thread:{0} --\n", t);
+                        Monitor.Wait(_Lock);
+                    }
+                    j = DeQueue();
+                    Console.WriteLine("## DeQueue j={0}, thread:{1} ##", j, t);
+                    Thread.Sleep(100);
+                    if(Count <= 9){ // if not Full, wake up EnQueue Thread
                         Monitor.Pulse(_Lock);
                     }
-
                 }
-
             }
         }
+
+        // EnQueue is **
+        // DeQueue is ##
+        // Waiting is --
+
         static void Main(string[] args)
         {
             Thread t1 = new Thread(th01);
-            //Thread t11 = new Thread(th011);
+            Thread t11 = new Thread(th011);
             Thread t2 = new Thread(th02);
-            //Thread t21 = new Thread(th02);
-            //Thread t22 = new Thread(th02);
+            Thread t21 = new Thread(th02);
+            Thread t22 = new Thread(th02);
 
-            t1.Start();
-            //t11.Start();
-            t2.Start(1);
-            //t21.Start(2);
-            //t22.Start(3);
+            t1.Start(1);
+            t11.Start(2);
+            t2.Start(3);
+            t21.Start(4);
+            t22.Start(5);
         }
     }
 }
